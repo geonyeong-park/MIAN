2a676eeb5f7a43ea089254bda8e05646db25176f
ParkGeonYeong <pky3436@kaist.ac.kr>
2020-05-23 20:44:11 +0900

    modified discriminator

--- model/discriminator.py
+++ model/discriminator.py
@@ -22,9 +22,9 @@ class OfficeDiscriminator(nn.Module):
         out_src = self.conv_domain_cls_patch(x)
         return out_src
 
-class OfficeDiscriminator(nn.Module):
+class DigitDiscriminator(nn.Module):
     def __init__(self, channel=4096, num_domain=3):
-        super(OfficeDiscriminator, self).__init__()
+        super(DigitDiscriminator, self).__init__()
 
         self.conv_domain_cls_patch = nn.Sequential(*[
             nn.Linear(channel, channel//2),
